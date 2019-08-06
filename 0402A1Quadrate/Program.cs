be104ae5b9a746f4a5bf1d271c5196e0fd23c267
frank paulsen <frobnic+ita@gmail.com>
2019-08-06 10:26:17 +0200

    Schleifen eingefügt

--- 0402A1Quadrate/Program.cs
+++ 0402A1Quadrate/Program.cs
@@ -10,6 +10,28 @@ namespace _0402A1Quadrate
     {
         static void Main(string[] args)
         {
+            int i = 0;
+
+            // for-schleife
+            for (i = 1; i <= 10; i++)
+                Console.WriteLine("Das Quadrat von {0,2} ist {1,3}", i, i * i);
+
+            // while-schleife
+            i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine("Das Quadrat von {0,2} ist {1,3}", i, i * i);
+                i++;
+            }
+
+            // do while-schleife
+            i = 1;
+            do
+            {
+                Console.WriteLine("Das Quadrat von {0,2} ist {1,3}", i, i * i);
+                i++;
+            } while (i <= 10);
+
         }
     }
 }
